c1c93dfea67032dd70603d3145d9ac5f6213223f
monkmitrad <monkmitrad@gmail.com>
2020-12-07 13:01:43 +0100

    added basic logic for login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +9,28 @@ import { NgForm } from "@angular/forms";
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(loginForm: NgForm) {
-    console.log(loginForm.value);
-    console.log(loginForm.valid);
+    if (loginForm.valid) {
+      const user: string = loginForm.value.name;
+      const id: string = loginForm.value.id;
+      console.log(user, id);
+
+      // TODO: API Request to Server to validate Game-ID and register User for game
+
+      const apiResponse: boolean = true;
+      const userID: number = 1337;
+      if (apiResponse) {
+        // login succesful and user registered for game
+        // save userID in localStorage and send to game
+        localStorage.setItem('userID', userID.toString());
+        this.router.navigate(['/game']);
+      }
+    }
   }
-  
+
 }
