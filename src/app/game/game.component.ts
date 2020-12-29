7ff25fac4ac662d71d097932a97e9fb86b82aa67
monkmitrad <monkmitrad@gmail.com>
2020-12-29 16:30:45 +0100

    changed input type of id, little changes to layout of game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -18,6 +18,7 @@ export class GameComponent implements OnInit, OnDestroy {
   gameStarted = false;
   ownUser: string;
   assignedUser: string;
+  gameID: number;
 
   // Subscriptions
   listPlayersSub: Subscription;
@@ -50,6 +51,7 @@ export class GameComponent implements OnInit, OnDestroy {
     */
 
     this.ownUser = this.gameService.getName();
+    this.gameID = this.gameService.getGameID();
   }
 
   ngOnDestroy(): void {
